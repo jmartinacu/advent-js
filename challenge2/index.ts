565cbead98a8cbecdc95d53aed194eb63a7ea39b
Joaquín Martín Acuña <jmartinacu2002@gmail.com>
2025-08-13 20:55:55 +0200

    feat: Completed challenge 2

---
+++ challenge2/index.ts
@@ -0,0 +1,42 @@
+function createFrame(names: string[]): string {
+    const borderChar = "*"
+    const maxLength = Math.max(...names.map(name => name.length + 4))
+    let result = borderChar.repeat(maxLength) + '\n'
+    for (const name of names) {
+        const paddedName = name.padEnd(maxLength - 4, ' ')
+        result += `${borderChar} ${paddedName} ${borderChar}\n`
+    }
+    result += borderChar.repeat(maxLength)
+    return result
+}
+
+
+console.log(createFrame(['midu', 'madeval', 'educalvolpz']))
+console.log("")
+
+// Resultado esperado:
+// ***************
+// * midu        *
+// * madeval     *
+// * educalvolpz *
+// ***************
+
+console.log(createFrame(['midu']))
+console.log("")
+
+// Resultado esperado:
+// ********
+// * midu *
+// ********
+
+console.log(createFrame(['a', 'bb', 'ccc']))
+console.log("")
+
+// Resultado esperado:
+// *******
+// * a   *
+// * bb  *
+// * ccc *
+// *******
+
+console.log(createFrame(['a', 'bb', 'ccc', 'dddd']))
